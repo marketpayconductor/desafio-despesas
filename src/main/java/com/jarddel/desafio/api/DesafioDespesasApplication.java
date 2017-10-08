f4965c6f030af710aa2a3407ae604098a0d3a132
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 13:20:01 -0300

    Adiciona estrutura base e dependências do spring boot.

---
+++ src/main/java/com/jarddel/desafio/api/DesafioDespesasApplication.java
@@ -0,0 +1,12 @@
+package com.jarddel.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDespesasApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jarddel/desafio/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jarddel.desafio.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DesafioDespesasApplication.class);
+	}
+
+}
