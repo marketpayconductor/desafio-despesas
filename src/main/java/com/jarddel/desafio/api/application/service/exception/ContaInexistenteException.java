eff6023327ebd01dc056a4ce6d6819f7e3ac8508
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 14:02:28 -0300

    Adiciona exceções de domínio e aplicação

---
+++ src/main/java/com/jarddel/desafio/api/application/service/exception/ContaInexistenteException.java
@@ -0,0 +1,7 @@
+package com.jarddel.desafio.api.application.service.exception;
+
+public class ContaInexistenteException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/jarddel/desafio/api/application/service/exception/UsuarioJaCadastradoException.java
@@ -0,0 +1,6 @@
+package com.jarddel.desafio.api.application.service.exception;
+
+public class UsuarioJaCadastradoException extends RuntimeException {
+
+    private static final long serialVersionUID = 4357906957255778770L;
+}
