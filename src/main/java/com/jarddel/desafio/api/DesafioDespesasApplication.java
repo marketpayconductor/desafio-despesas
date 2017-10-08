058bc68e677b95e7709da2e5caa13c913efbf67d
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 13:41:24 -0300

    Define e habilita conjunto de propriedades configuráveis específicas para o projeto

--- src/main/java/com/jarddel/desafio/api/DesafioDespesasApplication.java
+++ src/main/java/com/jarddel/desafio/api/DesafioDespesasApplication.java
@@ -2,11 +2,15 @@ package com.jarddel.desafio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.jarddel.desafio.api.application.configurations.ApplicationProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperty.class)
 public class DesafioDespesasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioDespesasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDespesasApplication.class, args);
+    }
 }
