fc29f97187ce0e4a88e5fa61f46edd5df70737e8
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 17:57:13 -0300

    Adiciona verificação de usuário ativo antes de persistir uma conta

--- src/main/java/com/jarddel/desafio/api/infrastructure/persistence/hibernate/listener/ContaListener.java
+++ src/main/java/com/jarddel/desafio/api/infrastructure/persistence/hibernate/listener/ContaListener.java
@@ -2,14 +2,28 @@ package com.jarddel.desafio.api.infrastructure.persistence.hibernate.listener;
 
 import javax.persistence.PrePersist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+import com.jarddel.desafio.api.domain.exception.UsuarioInativoException;
 import com.jarddel.desafio.api.domain.model.conta.Conta;
 
 public class ContaListener {
+
+    @Autowired
+    protected MessageSource messageSource;
 
     @PrePersist
     public void contaPrePersit(Conta conta) {
         conta.zerarSaldo();
         conta.defineDataDefault();
+        verificarUsuarioAtivo(conta);
+    }
+
+    public void verificarUsuarioAtivo(Conta conta) {
+        if (!conta.getUsuario().isAtivo()) {
+            throw new UsuarioInativoException();
+        }
     }
 
 }
