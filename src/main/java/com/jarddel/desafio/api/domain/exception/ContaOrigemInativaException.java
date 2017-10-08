58004c8a1556d26afed4fdbc6c67970c4f4c98c2
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 17:53:14 -0300

    Adiciona exceções decorrentes de usuario ou conta inativa.

---
+++ src/main/java/com/jarddel/desafio/api/domain/exception/ContaOrigemInativaException.java
@@ -0,0 +1,7 @@
+package com.jarddel.desafio.api.domain.exception;
+
+public class ContaOrigemInativaException extends LancamentoInvalidoException {
+
+    private static final long serialVersionUID = -2637935250087441975L;
+
+}
---
+++ src/main/java/com/jarddel/desafio/api/domain/exception/UsuarioInativoException.java
@@ -0,0 +1,7 @@
+package com.jarddel.desafio.api.domain.exception;
+
+public class UsuarioInativoException extends RuntimeException {
+
+    private static final long serialVersionUID = 402496343266007802L;
+
+}
