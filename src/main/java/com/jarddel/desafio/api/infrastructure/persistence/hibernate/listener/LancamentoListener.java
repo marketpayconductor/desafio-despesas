bd97c8e719ca57c46749a81a98531f0f632004e2
Jarddel Antunes <jarddel@hotmail.com>
2017-10-08 17:59:31 -0300

    Implementa verificação de conta ativa ao persistir um lançamento

--- src/main/java/com/jarddel/desafio/api/infrastructure/persistence/hibernate/listener/LancamentoListener.java
+++ src/main/java/com/jarddel/desafio/api/infrastructure/persistence/hibernate/listener/LancamentoListener.java
@@ -2,13 +2,21 @@ package com.jarddel.desafio.api.infrastructure.persistence.hibernate.listener;
 
 import javax.persistence.PrePersist;
 
+import com.jarddel.desafio.api.domain.exception.ContaDestinoInativaException;
 import com.jarddel.desafio.api.domain.model.lancamento.Lancamento;
 
 public class LancamentoListener {
 
     @PrePersist
-    public void defineDataDefaultPrePersit(Lancamento lancamento) {
+    public void lancamentoPrePersit(Lancamento lancamento) {
         lancamento.defineDataDefault();
+        verificarContaAtiva(lancamento);
+    }
+
+    public void verificarContaAtiva(Lancamento lancamento) {
+        if (!lancamento.getContaDestino().isAtivo()) {
+            throw new ContaDestinoInativaException();
+        }
     }
 
 }
