e25f080dcb53979b382e5b74cb3a03b876a7b200
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-09 20:21:24 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Desafio Conductor de Seleção
---
+++ src/main/java/br/pro/fagnerlima/desafiodespesas/DesafioDespesasApplication.java
@@ -0,0 +1,12 @@
+package br.pro.fagnerlima.desafiodespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDespesasApplication.class, args);
+	}
+}
