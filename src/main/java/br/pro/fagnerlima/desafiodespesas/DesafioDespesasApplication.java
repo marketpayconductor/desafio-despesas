1591dc052bfc842b792da55450935c2c1f383982
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-09 23:43:36 -0300

    Ajusta indentação

--- src/main/java/br/pro/fagnerlima/desafiodespesas/DesafioDespesasApplication.java
+++ src/main/java/br/pro/fagnerlima/desafiodespesas/DesafioDespesasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesafioDespesasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioDespesasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDespesasApplication.class, args);
+    }
 }
