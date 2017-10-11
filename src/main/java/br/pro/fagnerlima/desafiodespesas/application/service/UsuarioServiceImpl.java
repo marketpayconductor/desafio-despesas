785d2e5ced2956ee11636fdc79ff7c0161af6a9f
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-11 19:37:17 -0300

    Altera estratégia para criação de conta
    - Move a criação da conta do UsuarioService para UsuarioListener

--- src/main/java/br/pro/fagnerlima/desafiodespesas/application/service/UsuarioServiceImpl.java
+++ src/main/java/br/pro/fagnerlima/desafiodespesas/application/service/UsuarioServiceImpl.java
@@ -3,14 +3,11 @@ package br.pro.fagnerlima.desafiodespesas.application.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.pro.fagnerlima.desafiodespesas.application.service.exception.UsuarioNaoEncontradoException;
-import br.pro.fagnerlima.desafiodespesas.domain.model.conta.entity.Conta;
 import br.pro.fagnerlima.desafiodespesas.domain.model.usuario.entity.Usuario;
 import br.pro.fagnerlima.desafiodespesas.domain.service.UsuarioService;
 import br.pro.fagnerlima.desafiodespesas.infrastructure.persistence.hibernate.repository.BaseRepository;
-import br.pro.fagnerlima.desafiodespesas.infrastructure.persistence.hibernate.repository.ContaRepository;
 import br.pro.fagnerlima.desafiodespesas.infrastructure.persistence.hibernate.repository.UsuarioRepository;
 
 @Service
@@ -19,9 +16,6 @@ public class UsuarioServiceImpl extends BaseServiceImpl<Usuario, Long> implement
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private ContaRepository contaRepository;
-
     @Override
     public Usuario findOne(Long id) throws UsuarioNaoEncontradoException {
         try {
@@ -29,17 +23,6 @@ public class UsuarioServiceImpl extends BaseServiceImpl<Usuario, Long> implement
         } catch (EmptyResultDataAccessException exception) {
             throw new UsuarioNaoEncontradoException();
         }
-    }
-
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public Usuario save(Usuario usuario) {
-        usuario = super.save(usuario);
-
-        Conta conta = new Conta(usuario);
-        contaRepository.save(conta);
-
-        return usuario;
     }
 
     @Override
--- src/main/java/br/pro/fagnerlima/desafiodespesas/infrastructure/persistence/hibernate/listener/UsuarioListener.java
+++ src/main/java/br/pro/fagnerlima/desafiodespesas/infrastructure/persistence/hibernate/listener/UsuarioListener.java
@@ -6,23 +6,22 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 
 import br.pro.fagnerlima.desafiodespesas.application.util.PasswordEncoderUtils;
+import br.pro.fagnerlima.desafiodespesas.domain.model.conta.entity.Conta;
 import br.pro.fagnerlima.desafiodespesas.domain.model.usuario.entity.Usuario;
 
 public class UsuarioListener {
 
     @PrePersist
     public void usuarioPrePersist(Usuario usuario) {
+        usuario.setConta(new Conta(usuario));
         usuario.setDataCriacao(LocalDateTime.now());
-        setDefaultValues(usuario);
+        usuario.setAtivo(true);
+        usuario.setSenha(PasswordEncoderUtils.encode(usuario.getSenha()));
+        usuario.setDataAtualizacao(LocalDateTime.now());
     }
 
     @PreUpdate
     public void usuarioPreUpdate(Usuario usuario) {
-        setDefaultValues(usuario);
-    }
-
-    private void setDefaultValues(Usuario usuario) {
-        usuario.setAtivo(true);
         usuario.setSenha(PasswordEncoderUtils.encode(usuario.getSenha()));
         usuario.setDataAtualizacao(LocalDateTime.now());
     }
