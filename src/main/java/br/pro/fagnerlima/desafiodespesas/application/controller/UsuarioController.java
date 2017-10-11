8fff3f633efd272a5a29a71f4d42ea197ef4f57f
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-11 04:23:48 -0300

    Adiciona status HTTP no recurso delete do Usuario

--- src/main/java/br/pro/fagnerlima/desafiodespesas/application/controller/UsuarioController.java
+++ src/main/java/br/pro/fagnerlima/desafiodespesas/application/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.pro.fagnerlima.desafiodespesas.domain.model.usuario.entity.Usuario;
@@ -77,6 +78,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         usuarioService.delete(id);
     }
