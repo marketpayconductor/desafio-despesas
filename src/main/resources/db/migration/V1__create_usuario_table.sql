5c97856a961b4dab531602bf61f486ccd38a055e
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-11 03:51:44 -0300

    Atualiza as migrations

--- src/main/resources/db/migration/V1__create_usuario_table.sql
+++ src/main/resources/db/migration/V1__create_usuario_table.sql
@@ -11,3 +11,19 @@ CREATE TABLE usuario (
     CONSTRAINT pk_usuario PRIMARY KEY(id),
     CONSTRAINT uk_usuario_email UNIQUE KEY(email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE permissao (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_permissao PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE usuario_permissao (
+    id_usuario BIGINT NOT NULL,
+    id_permissao BIGINT NOT NULL,
+    CONSTRAINT pk_usuario_permissao PRIMARY KEY(id_usuario, id_permissao),
+    CONSTRAINT fk_usu_perm_usuario FOREIGN KEY(id_usuario) REFERENCES usuario(id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_usu_perm_permissao FOREIGN KEY(id_permissao) REFERENCES permissao(id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
